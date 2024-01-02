828cf90be028e4e7ebb8afc53c6cfcef7018dc16
Rayan Khan <iRayanKhan@icloud.com>
2024-01-01 22:29:41 -0600

    Initial Commit

---
+++ Mini Browser/Mini_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mini_BrowserApp.swift
+//  Mini Browser
+//
+//  Created by Rayan Khan on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Mini_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
