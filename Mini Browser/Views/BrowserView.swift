0fae6bdc43ca07e6395e7ba948ce08628b85d42a
Rayan Khan <iRayanKhan@icloud.com>
2024-01-01 22:31:38 -0600

    - MVVM setup for iOS

---
+++ Mini Browser/Views/BrowserView.swift
@@ -0,0 +1,18 @@
+//
+//  BrowserView.swift
+//  Mini Browser
+//
+//  Created by Rayan Khan on 1/1/24.
+//
+
+import SwiftUI
+
+struct BrowserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BrowserView()
+}
